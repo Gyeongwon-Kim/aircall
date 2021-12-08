04d7517913e2c6ba512b93359b74402b6f5cdba6
Gyeongwon Kim <gyeongwonkim@Gyeongwons-MacBook-Pro.local>
2021-12-08 02:55:06 -0500

    Call Detail Box initial update

--- src/components/activity/ActivityDetail.jsx
+++ src/components/activity/ActivityDetail.jsx
@@ -4,37 +4,40 @@ import Moment from 'moment';
 const ActivityDetail = ({activity}) => {
   return (
     activity !== null && (
-      <div className="detail-box">
-        {activity.call_type=="voicemail" ? (
-          <div className="content-box">
-            <i className={`material-icons icon-call color-${activity.call_type}`}>voicemail</i>
-            <div>
-              <div>{activity.from ? activity.from : 'Unknown'}</div>
-              <div>voice mail from {activity.to ? activity.to : 'Unknown'}</div>
-            </div>
-          </div>
-        ) : (
-          [activity.direction=="inbound" ? (
+      <div>
+        <div className="divider">{Moment(activity.created_at).format('MMM, D YYYY')}</div>
+        <div className="detail-box">
+          {activity.call_type=="voicemail" ? (
             <div className="content-box">
-              <i className={`material-icons icon-call color-${activity.call_type}`}>call_received</i>
+              <i className={`material-icons icon-call color-${activity.call_type}`}>voicemail</i>
               <div>
-                <div>{activity.from ? activity.from : 'Unknown'}</div>
-                <div>missed a call from {activity.to ? activity.to : 'Unknown'}</div>
+                <div className="phone-num">{activity.from ? activity.from : 'Unknown'}</div>
+                <div className="desc">voice mail from {activity.to ? activity.to : 'Unknown'}</div>
               </div>
             </div>
           ) : (
-            <div className="content-box">
-              <i className={`material-icons icon-call color-${activity.call_type}`}>call_made</i>
-              <div>
-                <div>{activity.to ? activity.to : 'Unknown'}</div>
-                <div>tried to call on {activity.from ? activity.from : 'Unknown'}</div>
+            [activity.direction=="inbound" ? (
+              <div className="content-box">
+                <i className={`material-icons icon-call color-${activity.call_type}`}>call_received</i>
+                <div>
+                  <div className="phone-num">{activity.from ? activity.from : 'Unknown'}</div>
+                  <div className="desc">missed a call from {activity.to ? activity.to : 'Unknown'}</div>
+                </div>
               </div>
+            ) : (
+              <div className="content-box">
+                <i className={`material-icons icon-call color-${activity.call_type}`}>call_made</i>
+                <div>
+                  <div className="phone-num">{activity.to ? activity.to : 'Unknown'}</div>
+                  <div className="desc">tried to call on {activity.from ? activity.from : 'Unknown'}</div>
+                </div>
+              </div>
+            )]
+          )}
+          <div className="time-box">
+            <div>
+              {Moment(activity.created_at).format('hh:mm a')}
             </div>
-          )]
-        )}
-        <div className="time-box">
-          <div>
-            {Moment(activity.created_at).format('hh:mm a')}
           </div>
         </div>
       </div>
