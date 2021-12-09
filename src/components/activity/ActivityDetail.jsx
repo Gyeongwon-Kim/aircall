0a4ec7018b64aaa89d5cf3408b54887ac80b2d54
Gyeongwon Kim <gyeongwonkim@Gyeongwons-MacBook-Pro.local>
2021-12-09 02:02:04 -0500

    Design Improvement

--- src/components/activity/ActivityDetail.jsx
+++ src/components/activity/ActivityDetail.jsx
@@ -4,7 +4,7 @@ import Moment from 'moment';
 import { useParams } from 'react-router-dom';
 
 
-const ActivityDetail = (props) => {
+const ActivityDetail = () => {
     const { id } = useParams();
     
     const [activity, setActivity] = useState([]);
@@ -34,18 +34,30 @@ const ActivityDetail = (props) => {
             }
         })
     }
+
     return (
         <div className="detail-container">
             <div><i className="material-icons icon-user">account_circle</i></div>
             <div className="detail-username">{activity.direction=="outbound" ? (activity.from ? activity.from : 'Unknown') : (activity.to ? activity.to : 'Unknown')}</div>
             <div className="detail-phone-num">{activity.direction=="outbound" ? (activity.to ? activity.to : 'Unknown') : (activity.from ? activity.from : 'Unknown')}</div>
             <div className="detail-content-box">
-                <div>{Moment(activity.created_at).format('MMM d, YYYY')}</div>
-                <div>{activity.direction}</div>
-                <div>{activity.call_type}</div>
-                <div>via {activity.via}</div>
-                <div>{activity.duration} secs</div>
-                <div>{activity.is_archived ? 'Archived' : 'Not archived'}</div>
+                <div>
+                    <div>Date</div>
+                    <div>Direction</div>
+                    <div>Status</div>
+                    <div>Via</div>
+                    <div>Duration</div>
+                    <div>Archive Status</div>
+                </div>
+                <div className="vertical-divider"></div>
+                <div>
+                    <div>{Moment(activity.created_at).format('MMM d, YYYY')}</div>
+                    <div>{activity.direction}</div>
+                    <div>{activity.call_type}</div>
+                    <div>{activity.via}</div>
+                    <div>{activity.duration} secs</div>
+                    <div>{activity.is_archived ? 'Archived' : 'Not archived'}</div>
+                </div>
             </div>
             <button className="button-archive" onClick={() => updateArchiveStatus(!activity.is_archived)}>
                 {activity.is_archived ? 'Unarchive' : 'Archive'}
